a483501e65e588d682cc3bdef2ca7a25e90f22f6
Karen Payne <paynekaren@comcast.net>
2023-09-18 04:41:59 -0700

    updated to Framework 4.8

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.excelHelper1 = new ExcelTrayLibrary.ExcelHelper();
+            this.SuspendLayout();
+            // 
+            // excelHelper1
+            // 
+            this.excelHelper1.FileName = null;
+            this.excelHelper1.Headers = ExcelTrayLibrary.UseHeader.YES;
+            this.excelHelper1.Mode = ExcelTrayLibrary.MixType.AsText;
+            this.excelHelper1.Provider = ExcelTrayLibrary.ExcelProvider.XLS;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ExcelTrayLibrary.ExcelHelper excelHelper1;
+    }
+}
+
